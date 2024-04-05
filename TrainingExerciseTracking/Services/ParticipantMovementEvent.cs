91e8056d317365e58e4cfe2661f2179f5c3e1720
Teddy55Codes <teddy@teddy.email>
2024-04-05 16:20:52 +0200

    added aggrigate event

---
+++ TrainingExerciseTracking/Services/ParticipantMovementEvent.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Events;
+using TrainingExerciseTracking.Database.Models;
+
+namespace TrainingExerciseTracking.Services;
+
+public class ParticipantMovementEvent : PubSubEvent<Movement>
+{
+    
+}
