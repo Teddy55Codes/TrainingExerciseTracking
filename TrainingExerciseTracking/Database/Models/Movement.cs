47719d938d3a8211bc9fc9f1656c607acb24a89e
Teddy55Codes <teddy@teddy.email>
2024-04-05 15:58:28 +0200

    added database models

---
+++ TrainingExerciseTracking/Database/Models/Movement.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingExerciseTracking.Database.Models;
+
+public class Movement
+{
+    public long Id { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    
+    public virtual int ParticipantId { get; set; }
+    public virtual Participant Participant { get; set; }
+}
---
+++ TrainingExerciseTracking/Database/Models/Participant.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingExerciseTracking.Database.Models;
+
+public class Participant
+{
+    public int Id { get; set; }
+    public string Rank { get; set; }
+    public string Country { get; set; }
+    public string Information { get; set; }
+    
+    public virtual  List<Movement> Movements { get; set; }
+}
