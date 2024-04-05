b0a1fe20ff666e0f09966718b2a62f8f07113fbb
Teddy55Codes <teddy@teddy.email>
2024-04-05 15:35:38 +0200

    added prism

--- TrainingExerciseTracking/App.xaml.cs
+++ TrainingExerciseTracking/App.xaml.cs
@@ -1,12 +1,22 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using TrainingExerciseTracking.Services;
 
 namespace TrainingExerciseTracking;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : PrismApplication
 {
+    /// <inheritdoc />
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.Register<MainWindow>();
+    }
+
+    /// <inheritdoc />
 }
